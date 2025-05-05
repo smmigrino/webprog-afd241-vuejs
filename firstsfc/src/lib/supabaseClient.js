c1aad6ae07b6140b6427909f05795620a201e2d5
smmigrino <smmigrino@student.apc.edu.ph>
2025-05-05 07:16:27 +0000

    supabase connection

---
+++ firstsfc/src/lib/supabaseClient.js
@@ -0,0 +1,4 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient('https://mmcejyysrzbxglsgbckc.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1tY2VqeXlzcnpieGdsc2diY2tjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMwNTg5NDYsImV4cCI6MjA1ODYzNDk0Nn0.zTc61VBV_ShM4mkoPoLImFcU51rSpZ5n7WEJ9EmWxH4')
+
