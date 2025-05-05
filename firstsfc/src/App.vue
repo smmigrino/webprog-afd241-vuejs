3cdccac8006ef22bd7cb8963a2e44f69b38f6488
smmigrino <smmigrino@student.apc.edu.ph>
2025-05-05 07:23:20 +0000

    supabase connect 2

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -15,51 +15,7 @@ onMounted(() => {
 </script>
 
 <template>
-<<<<<<< HEAD
   <ul>
     <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
   </ul>
 </template>
-=======
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="Yeeeeeey" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
-</template>
-
-<style scoped>
-header {
-  line-height: 3;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
->>>>>>> 602655dff84004b0aa0937081b6846e5550138b7
