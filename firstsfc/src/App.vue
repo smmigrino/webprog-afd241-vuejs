da73949fb4559b17d2f38ef2791d57de38a6c0b9
smmigrino <smmigrino@student.apc.edu.ph>
2025-05-19 02:56:19 +0000

    added

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -3,12 +3,27 @@
   <h2>Food</h2>
   <food-item/>
   <food-item2/>
-
- 
+    <ul>
+    <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
+  </ul>
 </template>
  
-<script></script>
+ 
+<script setup>
+import { ref, onMounted } from 'vue'
+import { supabase } from './lib/supabaseClient'
+ 
+const instruments = ref([])
+ 
+async function getInstruments() {
+  const { data } = await supabase.from('instruments').select()
+  instruments.value = data
+}
+ 
+onMounted(() => {
+   getInstruments()
+})
+</script>
  
 <style>
-
 </style>
--- firstsfc/src/lib/supabaseClient.js
+++ firstsfc/src/lib/supabaseClient.js
@@ -1,4 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
 export const supabase = createClient('https://mmcejyysrzbxglsgbckc.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1tY2VqeXlzcnpieGdsc2diY2tjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMwNTg5NDYsImV4cCI6MjA1ODYzNDk0Nn0.zTc61VBV_ShM4mkoPoLImFcU51rSpZ5n7WEJ9EmWxH4')
 
